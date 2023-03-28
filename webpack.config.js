a120505fb17e51ee1db43558bc677150add4a55a
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-28 21:31:52 +0200

    Linters configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-          }
-        ]
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", "index.html")
-      })
-    ]
-  };
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
