35feb6ead9b3deb856f2e9d5fbafe2059d309a5c
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-29 11:53:25 +0200

    Add JS objects and for loop and also add HTM elements and gave style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const tasks = [
+  {
+    description: 'Going to gym',
+    complated: false,
+    index: 1,
+  },
+  {
+    description: 'Cook dinner',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Tidy up the house',
+    completed: false,
+    index: 3,
+  },
+];
+
+const taskList = document.querySelector('.taskList');
+
+const generateTask = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    const tsk = tasks[i];
+    taskList.innerHTML += `<li class="task flex">
+    <input type="checkbox" id="checked" name="checked" >
+    <label class="label" id="label" for="checked">${tsk.description}</label>
+    <span>&#8285;</span>
+   </li>`;
+  }
+};
+
+window.onload = generateTask();
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
